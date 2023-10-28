ea5dcba35f173c5c21d22c016fcc050b07395707
Katrina <146173650+katrinaMalkova@users.noreply.github.com>
2023-10-28 12:19:04 +0000

    обьяснение

--- task2.js
+++ task2.js
@@ -2,8 +2,8 @@
 //Создайте функцию celsiusToFahrenheit(celsius), которая принимает температуру в градусах Цельсия и возвращает ее эквивалент в градусах Фаренгейта. Формула: (C * 9/5) + 32.
 
 function celsiusToFahrenheit(celsius) {
-    var fahrenheit = (celsius * 9/5) + 32;
-    return fahrenheit;
+    var fahrenheit = (celsius * 9/5) + 32; //Внутри функции происходит вычисление температуры в градусах Фаренгейта.
+    return fahrenheit; // Этот оператор возвращает значение fahrenheit из функции
   }
 
 //var celsius = 25;
--- task3.js
+++ task3.js
@@ -2,8 +2,8 @@
 //Напишите функцию calculateDiscount(price, discountPercentage), которая принимает цену товара и процент скидки, а затем возвращает сумму с учетом скидки.
 
 function calculateDiscount(price, discountPercentage) {
-    var discountAmount = (price * discountPercentage) / 100;
-    var discountedPrice = price - discountAmount;
+    var discountAmount = (price * discountPercentage) / 100; //  сумма скидки вычисляется как произведение цены товара на процент скидки и делится на 100. Это представляет собой сумму, на которую снизится исходная цена.
+    var discountedPrice = price - discountAmount; //
     return discountedPrice;
   }
   
